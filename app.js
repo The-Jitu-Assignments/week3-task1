2cccac2682472ff97987928a12f44c50101caa13
john katua <johnkatua99@gmail.com>
2022-10-23 08:16:46 +0300

    filtering between complete and incomplete tasks

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const todoContainer = document.querySelector('.todo--content');
+const addBtn = document.querySelector('.add--btn');
+const textInput = document.querySelector('.text--input');
+const completedTodos = document.querySelector('.completed--todos');
+const inCompleteTodos = document.querySelector('.incomplete--todos');
+const todoTitle = document.querySelector('.todo--title');
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 const fetchData = (url) => {
@@ -10,6 +17,98 @@ const fetchData = (url) => {
 fetchData(url);
 
 const getTodos = (todos) => {
-  console.log(todos);
+  let myTodos = todos.slice(0, 10);
+  console.log(myTodos);
+  displayTodos(myTodos);
+  allTodos(myTodos);
+
+  let inCompleteTodos = myTodos.filter((todo) => todo.completed === false);
+  let completedTodos = myTodos.filter((todo) => todo.completed === true);
+  
+  getIncompleteTodos(inCompleteTodos);
+  getCompletedTodos(completedTodos);
+  addNewTodo(myTodos);
 }
+
+const displayTodos = (todos) => {
+  let list = '';
+  todos.map((todo) => {
+    list += `
+    <div class="todo--item">
+      <div class="item--title">${todo.title}</div>
+      <div class="item--status">${checkTodoStatus(todo.completed)}</div>
+    </div>
+    `
+  });
+  todoContainer.innerHTML = list
+}
+
+const checkTodoStatus = value => {
+  let status = '';
+  value === true ? (status = 'COMPLETED') : (status = 'UNCOMPLETED');
+  return status
+}
+
+// const addData = async (url, data) => {
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     body: JSON.stringify(data)
+//   })
+//   return response.json();
+// }
+
+// addBtn.onclick = () => {
+//   let todo = {
+//     title: textInput.value,
+//     completed: true,
+//     userId: 2
+//   };
+
+//   console.log(todo);
+
+//   let data = addData(url, todo)
+//   console.log(data.then((todo) => console.log(todo)))
+//   textInput.value = '';
+// }
+
+const getIncompleteTodos = (todos) => {
+  inCompleteTodos.onclick = () => {
+    displayTodos(todos);
+    inCompleteTodos.classList.add('active');
+    completedTodos.classList.remove('active')
+  }
+};
+
+const getCompletedTodos = (todos) => {
+  completedTodos.onclick = () => {
+    displayTodos(todos);
+    completedTodos.classList.add('active');
+    inCompleteTodos.classList.remove('active');
+  }
+}
+
+const addNewTodo = (todos) => {
+  addBtn.onclick = () => {
+      let newTodo = {
+        title: textInput.value,
+        completed: true,
+        userId: 2
+      }
+      todos.push(newTodo);
+      displayTodos(todos);
+      textInput.value = '';
+      inCompleteTodos.classList.remove('active');
+      completedTodos.classList.remove('active');
+  }
+};
+
+const allTodos = todos => {
+  todoTitle.onclick = () => {
+    console.log(todos)
+    displayTodos(todos);
+    inCompleteTodos.classList.remove('active');
+    completedTodos.classList.remove('active');
+  }
+};
+
 
