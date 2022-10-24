2d5424e491cd5f7bc593c9b61c6ea48ac12b32af
john katua <johnkatua99@gmail.com>
2022-10-24 03:03:02 +0300

    added pagination to the project

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const textInput = document.querySelector('.text--input');
 const completedTodos = document.querySelector('.completed--todos');
 const inCompleteTodos = document.querySelector('.incomplete--todos');
 const todoTitle = document.querySelector('.todo--title');
+const paginationNumbers = document.getElementById("pagination-numbers");
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
+
+let currentPage;
+const paginationLimit = 15;
+let page = 0;
+
+const pageCount = data => {
+  return Math.ceil(data.length / paginationLimit)
+};
+
 
 const fetchData = (url) => {
   fetch(url)
@@ -17,18 +27,20 @@ const fetchData = (url) => {
 fetchData(url);
 
 const getTodos = (todos) => {
-  let myTodos = todos.slice(0, 10);
-  console.log(myTodos);
-  displayTodos(myTodos);
-  allTodos(myTodos);
+  displayTodos(todos);
+  allTodos(todos);
 
-  let inCompleteTodos = myTodos.filter((todo) => todo.completed === false);
-  let completedTodos = myTodos.filter((todo) => todo.completed === true);
+  let inCompleteTodos = todos.filter((todo) => todo.completed === false);
+  let completedTodos = todos.filter((todo) => todo.completed === true);
   
   getIncompleteTodos(inCompleteTodos);
   getCompletedTodos(completedTodos);
-  addNewTodo(myTodos);
-}
+  addNewTodo(todos);
+
+  page = pageCount(todos)
+  getPaginationNumbers(page);
+};
+
 
 const displayTodos = (todos) => {
   let list = '';
@@ -40,7 +52,10 @@ const displayTodos = (todos) => {
     </div>
     `
   });
-  todoContainer.innerHTML = list
+  todoContainer.innerHTML = list;
+  let listItem = document.querySelectorAll('.todo--item');
+  setCurrentPage(1, listItem);
+  updatePage(listItem)
 }
 
 const checkTodoStatus = value => {
@@ -49,27 +64,6 @@ const checkTodoStatus = value => {
   return status
 }
 
-// const addData = async (url, data) => {
-//   const response = await fetch(url, {
-//     method: 'POST',
-//     body: JSON.stringify(data)
-//   })
-//   return response.json();
-// }
-
-// addBtn.onclick = () => {
-//   let todo = {
-//     title: textInput.value,
-//     completed: true,
-//     userId: 2
-//   };
-
-//   console.log(todo);
-
-//   let data = addData(url, todo)
-//   console.log(data.then((todo) => console.log(todo)))
-//   textInput.value = '';
-// }
 
 const getIncompleteTodos = (todos) => {
   inCompleteTodos.onclick = () => {
@@ -111,4 +105,80 @@ const allTodos = todos => {
   }
 };
 
+const appendPageNumber = index => {
+  const pageNumber = document.createElement('button');
+  pageNumber.className = 'pagination-number';
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute('page-index', index);
+  pageNumber.setAttribute('aria-label', 'Page ' + index);
+
+  paginationNumbers.appendChild(pageNumber);
+}
+
+
+const getPaginationNumbers = (pages) => {
+  for (let i = 1; i <= pages; i++) {
+    appendPageNumber(i)
+  }
+}
+
+const setCurrentPage = (pageNum, data) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  data.forEach((item, index) => {
+    item.classList.add('hidden');
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove('hidden')
+    }
+  })
+}
+
+const updatePage = (data) => {
+  document.querySelectorAll('.pagination-number').forEach((button) => {
+    const pageIndex = Number(button.getAttribute('page-index'));
+
+    if (pageIndex) {
+      button.addEventListener('click', () => {
+        setCurrentPage(pageIndex, data)
+      })
+    }
+  })
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll('.pagination-number').forEach((button) => {
+    button.classList.remove('active');
+
+    const pageIndex = Number(button.getAttribute('page-index'));
+    if (pageIndex === currentPage) {
+      button.classList.add('active');
+    }
+  })
+}
+
+window.addEventListener('load', () => {
+  const displayTodos = (todos) => {
+    let list = '';
+    todos.map((todo) => {
+      list += `
+      <div class="todo--item">
+        <div class="item--title">${todo.title}</div>
+        <div class="item--status">${checkTodoStatus(todo.completed)}</div>
+      </div>
+      `
+    });
+    todoContainer.innerHTML = list;
+    let listItem = document.querySelectorAll('.todo--item');
+    setCurrentPage(1, listItem);
+    updatePage(listItem)
+  }
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayTodos(data));
+})
 
